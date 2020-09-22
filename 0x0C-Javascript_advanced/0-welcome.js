f19fc1636423c3b73aa34dd84b78cc9ce1304abc
Victor Hernandez <vik407@gmail.com>
2020-09-21 21:04:30 -0500

    welcome

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,5 @@
+const welcome = (firstName, lastName) => {
+    const fullName = `${firstName} ${lastName}`;
+    const displayFullName = () => alert(`Welcome ${fullName}`);
+    displayFullName();
+};
