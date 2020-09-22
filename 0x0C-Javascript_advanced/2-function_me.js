845d8562d0830e6ea0b2a3328e0f0b67e5e0baec
Victor Hernandez <vik407@gmail.com>
2020-09-21 21:40:56 -0500

    closure

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,4 @@
+const welcomeMessage = (fullName) => alert(`Welcome ${fullName}`);
+const guillaume = () => welcomeMessage("Guillaume");
+const alex = () => welcomeMessage("Alex");
+const fred = () => welcomeMessage("Fred");
